fcc4110d87719cc06b57d8448506f309e8674ccb
Robert Cernak <robert.cernak@eleveo.com>
2024-09-12 09:35:35 +0200

    283 - move zeros - easy

--- src/main/kotlin/com/leetcode/roberto/tasks/Task283MoveZerosEasy.kt
+++ src/main/kotlin/com/leetcode/roberto/tasks/Task283MoveZerosEasy.kt
@@ -34,6 +34,76 @@ class Task283MoveZerosEasy {
         println(nums.contentToString())
     }
 
+
+    /**
+     * in: 0, 1, 2, 3
+     * out: 1, 2, 3, 0
+     *
+     * in: 0, 1, 2, 0, 3, 0
+     * out: 1, 2, 3, 0, 0, 0
+     *
+     * ec:
+     * what if no zero? no problem
+     * if list empty? can not happend because of task constraints
+     *
+     * range is? -int max to +int max
+     *
+     *
+     * intuition:
+     * we need to go through array from left to right,
+     * if element is zero, then move it to the end of array - incorrect - will have to move rest of array 1 position to the left
+     * if element is zero, switch with next?
+     *
+     * ex1:
+     * l r
+     * 0,1,2,3
+     *   l r
+     * 1,0,2,3
+     *     l r
+     * 1,2,0,3
+     *       lr
+     * 1,2,3,0
+     *
+     *
+     * ex2:
+     * l r
+     * 0,1,2,0,3,0
+     *   l r
+     * 1,0,2,0,3,0
+     *     l r
+     * 1,2,0,0,3,0
+     * !!! r should move to next non zero number !!!
+     *     l   r
+     * 1,2,0,0,3,0
+     *       l   r
+     * 1,2,3,0,0,0
+     *
+     *
+     * ex3:
+     *   l r
+     * -10,5,0,1,0,2
+     * !!! if l and r are not 0, skip shifting
+     *     l   r
+     * -10,5,0,1,0,2
+     *       l r
+     * -10,5,0,1,0,2
+     *       l     r
+     * -10,5,1,0,0,2
+     *         l   r
+     * -10,5,1,0,0,2
+     *         l   r
+     * -10,5,1,2,0,0
+     *
+     *
+     * pseudocode:
+     *
+     * have 2 pointers l and r
+     * l starts from left and moves 1 element to to left
+     * r start from l+1 and moves to first non-zero number
+     * if l and r have non-zero number, continue
+     * if l has zero, switch with r and r moves to next non-zero number
+     * if r is at the end of array and there is no more non-zero numbers, finish
+     */
     class Solution {
         fun moveZeroes(nums: IntArray): Unit {
             var l = 0
